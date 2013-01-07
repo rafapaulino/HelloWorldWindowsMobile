b099ce94630f9dc6eab7b30be5650c5c0a8a5456
Rafael Paulino <rafaclasses@gmail.com>
2013-01-07 13:18:20 -0200

    Windows Mobile

---
+++ HelloWorld/MainPage.xaml.vb
@@ -0,0 +1,11 @@
+﻿Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+    ' Constructor
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+    Private Sub Button1_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        MessageBox.Show("Olá mundo!")
+    End Sub
+End Class
